8ef8e217acb121a4bddecf61424833e32bd87940
kevinrogerteng <kevin.roger.teng@getchute.com>
2017-01-30 13:49:18 -0800

    updated webpack production config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -4,21 +4,20 @@ var path = require('path')
 var webpack = require('webpack')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
-var StatsPlugin = require('stats-webpack-plugin')
 
 module.exports = {
   entry: [
-    path.join(__dirname, 'app/main.js')
+    path.join(__dirname, 'client/main.js')
   ],
   output: {
     path: path.join(__dirname, '/dist/'),
-    filename: '[name]-[hash].min.js',
-    publicPath: '/'
+    publicPath: '/',
+    filename: '[name]-[hash].min.js'
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new HtmlWebpackPlugin({
-      template: 'app/index.tpl.html',
+      template: 'client/index.tpl.html',
       inject: 'body',
       filename: 'index.html'
     }),
@@ -28,10 +27,6 @@ module.exports = {
         warnings: false,
         screw_ie8: true
       }
-    }),
-    new StatsPlugin('webpack.stats.json', {
-      source: false,
-      modules: false
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
@@ -43,7 +38,7 @@ module.exports = {
       exclude: /node_modules/,
       loader: 'babel',
       query: {
-        'presets': ['es2015', 'stage-0', 'react']
+        'presets': ['react', 'es2015', 'stage-0']
       }
     }, {
       test: /\.json?$/,
@@ -52,8 +47,9 @@ module.exports = {
       test: /\.css$/,
       loader: ExtractTextPlugin.extract('style', 'css?modules&localIdentName=[name]---[local]---[hash:base64:5]!postcss')
     }]
-  },
-  postcss: [
-    require('autoprefixer')
-  ]
+  }
+  // ,
+  // postcss: [
+  //   require('autoprefixer')
+  // ]
 }
